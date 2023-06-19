46c58ebb305c9f2d789cbdb625d8efbe9bf2aef7
kareem Re <lekareem21@outlook.com>
2023-06-19 22:31:09 +0300

    initial commit

---
+++ routes/Dashboard/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\dashboard\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('dashboard')->name('dashboard.')->group(function() {
+
+
+    Route::get('/index', [DashboardController::class, 'index'])->name('index');
+
+
+});
